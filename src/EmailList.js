9f99e74575a4a066f42ee0b5fb99a420a3ca5b9a
Dhruvin Patel <dhruvinpatel1831@gmail.com>
2020-12-26 00:07:46 +0530

    Email list and Rows added

--- src/EmailList.js
+++ src/EmailList.js
@@ -7,9 +7,11 @@ import {
   MoreVert,
   Redo,
   Settings,
+  People,
+  LocalOffer,
 } from "@material-ui/icons";
 import InboxIcon from "@material-ui/icons/Inbox";
-
+import EmailRow from "./EmailRow";
 import React from "react";
 import "./EmailList.css";
 import Section from "./Section";
@@ -46,7 +48,29 @@ function EmailList() {
         </div>
       </div>
       <div className="emailList__sections">
-        <Section Icon={InboxIcon} title="primary" color="red" selected />
+        <Section Icon={InboxIcon} title="Primary" color="red" selected />
+        <Section Icon={People} title="Social" color="#1A73EB" />
+        <Section Icon={LocalOffer} title="Promotions" color="green" />
+      </div>
+      <div className="emailList__list">
+        <EmailRow
+          title="Hello"
+          subject="hey there"
+          description="today is holidar"
+          time="12am"
+        />
+        <EmailRow
+          title="HelloemailRow__timeemailRow__timeemailRow__timeemailRow__timeemailRow__time"
+          subject="hey there"
+          description="today is holidaremailRow__titleemailRow__titleemailRow__titleemailRow__titleemailRow__titleemailRow__titleemailRow__title"
+          time="12am"
+        />
+        <EmailRow
+          title="Hello"
+          subject="hey there"
+          description="today is holidar"
+          time="12am"
+        />
       </div>
     </div>
   );
