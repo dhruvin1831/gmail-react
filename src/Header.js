2d6bb2a701e969086e994b68cc64c05054bc201c
Dhruvin Patel <dhruvinpatel1831@gmail.com>
2020-12-25 12:35:11 +0530

    Header added

---
+++ src/Header.js
@@ -0,0 +1,47 @@
+import React from "react";
+import "./Header.css";
+
+import SearchIcon from "@material-ui/icons/Search";
+import MenuIcon from "@material-ui/icons/Menu";
+import ArrowDropDownIcon from "@material-ui/icons/ArrowDropDown";
+import AppsIcon from "@material-ui/icons/Apps";
+import NotificationsIcon from "@material-ui/icons/Notifications";
+import { Avatar, IconButton } from "@material-ui/core";
+
+function Header() {
+  return (
+    <div className="header">
+      <div className="header__left">
+        <IconButton>
+          <MenuIcon />
+        </IconButton>
+        <img
+          src="https://i.pinimg.com/564x/d7/4e/60/d74e6036cb5b6e80d87b0a3fc77176c0.jpg"
+          alt=""
+        />
+      </div>
+
+      <div className="header__middle">
+        <IconButton>
+          <SearchIcon />
+        </IconButton>
+        <input placeholder="Search Mail" type="text" />
+        <IconButton>
+          <ArrowDropDownIcon className="header__inputCaret" />
+        </IconButton>
+      </div>
+
+      <div className="header__right">
+        <IconButton>
+          <AppsIcon />
+        </IconButton>
+        <IconButton>
+          <NotificationsIcon />
+        </IconButton>
+        <Avatar />
+      </div>
+    </div>
+  );
+}
+
+export default Header;
